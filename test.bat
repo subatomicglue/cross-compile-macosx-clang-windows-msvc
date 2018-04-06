ca86c5309c7eac4be1b0e5744780bb64c1e3af12
km <kmeinert@aviahealthinnovation.com>
2018-04-06 09:17:14 -0500

    added clang-cl tests

--- test.bat
+++ test.bat
@@ -5,6 +5,10 @@ mainCPP-x64.exe
 mainC-x64.exe
 mainCPP-x86.exe
 mainC-x86.exe
+mainCPP-x64-cl.exe
+mainC-x64-cl.exe
+mainCPP-x86-cl.exe
+mainC-x86-cl.exe
 libmain-x64.exe
 libmain-x86.exe
 dllmain-x64.exe
