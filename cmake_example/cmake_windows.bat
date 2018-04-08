62957e66fbd50552aa32d73109a0cf26fd305713
km <kmeinert@aviahealthinnovation.com>
2018-04-08 17:44:28 -0500

    added debug version for windows

--- cmake_example/cmake_windows.bat
+++ cmake_example/cmake_windows.bat
@@ -15,9 +15,10 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 rmdir /S /Q build-win64
 mkdir build-win64 & pushd build-win64
 cmake -DCMAKE_TOOLCHAIN_FILE=Modules\Native64.cmake -G "Visual Studio 14 Win64" ..
+cmake --build . --config Debug
+cmake --build . --config Release
 popd
 
-cmake --build build-win64 --config Release
 
 endlocal
 
@@ -30,9 +31,9 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 rmdir /S /Q build-win32
 mkdir build-win32 & pushd build-win32
 cmake -DCMAKE_TOOLCHAIN_FILE=Modules\Native32.cmake -G "Visual Studio 14" ..
+cmake --build . --config Debug
+cmake --build . --config Release
 popd
-
-cmake --build build-win32 --config Release
 
 endlocal
 
