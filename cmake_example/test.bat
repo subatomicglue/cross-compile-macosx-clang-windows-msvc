ea59c85f6b18fbdba56ba21339d6e2844645f399
km <kmeinert@aviahealthinnovation.com>
2018-04-07 19:43:16 -0500

    added script of tests for crosscompile - run these on windows after crosscompiling in macosx

---
+++ cmake_example/test.bat
@@ -0,0 +1,19 @@
+
+::"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\dumpbin" /headers *.exe
+
+build-wincross32\mainCPP-x86.exe
+build-wincross32\mainC-x86.exe
+build-wincross32\mainCPP-x86-cl.exe
+build-wincross32\mainC-x86-cl.exe
+build-wincross32\libmain-x86.exe
+build-wincross32\dllmain-x86.exe
+build-wincross32\pluginmain-x86.exe
+
+build-wincross64\mainCPP-x64.exe
+build-wincross64\mainC-x64.exe
+build-wincross64\mainCPP-x64-cl.exe
+build-wincross64\mainC-x64-cl.exe
+build-wincross64\libmain-x64.exe
+build-wincross64\dllmain-x64.exe
+build-wincross64\pluginmain-x64.exe
+
