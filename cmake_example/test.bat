4ea208017e92868ca5686b32a632cbe2691d2c66
km <kmeinert@aviahealthinnovation.com>
2018-04-12 08:32:10 -0500

    organize test scripts

--- cmake_example/test.bat
+++ cmake_example/test.bat
@@ -1,19 +1,38 @@
 
-::"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\dumpbin" /headers *.exe
+:: windows binary tests (to test native, go run ./test under a bash shell)
+:: run test.bat under cmd.exe
+::
+::    C:\cross-windows> test.bat
+::
+:: to test the architecture of the .exe files:
+::
+::   "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\dumpbin" /headers *.exe
+::
+
+
 
 build-wincross32\mainCPP-x86.exe
-build-wincross32\mainC-x86.exe
-build-wincross32\mainCPP-x86-cl.exe
-build-wincross32\mainC-x86-cl.exe
-build-wincross32\libmain-x86.exe
-build-wincross32\dllmain-x86.exe
-build-wincross32\pluginmain-x86.exe
-
 build-wincross64\mainCPP-x64.exe
+
+build-wincross32\mainC-x86.exe
 build-wincross64\mainC-x64.exe
+
+build-wincross32\mainCPP-x86-cl.exe
 build-wincross64\mainCPP-x64-cl.exe
+
+build-wincross32\mainC-x86-cl.exe
 build-wincross64\mainC-x64-cl.exe
+
+build-wincross32\libmain-x86.exe
 build-wincross64\libmain-x64.exe
+
+build-wincross32\dllmain-x86.exe
 build-wincross64\dllmain-x64.exe
+
+:: in bash shell, you have to run from the directory .exe is in
+:: in order for the plugin to be found in same dir
+:: seems not to be an issue for cmd.exe however.
+
+build-wincross32\pluginmain-x86.exe
 build-wincross64\pluginmain-x64.exe
 
--- makefile_example/test.bat
+++ makefile_example/test.bat
@@ -1,18 +1,32 @@
 
-::"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\dumpbin" /headers *.exe
+:: windows binary tests (to test native, go run ./test under a bash shell)
+:: run test.bat under cmd.exe
+::
+::    C:\cross-windows> test.bat
+::
+:: to test the architecture of the .exe files:
+::
+::   "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\dumpbin" /headers *.exe
+::
+
+mainC-x86.exe
+mainC-x64.exe
+
+mainC-x86-cl.exe
+mainC-x64-cl.exe
 
 mainCPP-x64.exe
-mainC-x64.exe
 mainCPP-x86.exe
-mainC-x86.exe
-mainCPP-x64-cl.exe
-mainC-x64-cl.exe
+
 mainCPP-x86-cl.exe
-mainC-x86-cl.exe
-libmain-x64.exe
+mainCPP-x64-cl.exe
+
 libmain-x86.exe
-dllmain-x64.exe
+libmain-x64.exe
+
 dllmain-x86.exe
-pluginmain-x64.exe
+dllmain-x64.exe
+
 pluginmain-x86.exe
+pluginmain-x64.exe
 
